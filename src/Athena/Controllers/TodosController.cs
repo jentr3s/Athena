80426369f391bf205145993a414312c353ea8842
jmacaso <jmacaso.vss@gmail.com>
2016-12-02 11:49:18 +0800

    Add project files.

---
+++ src/Athena/Controllers/TodosController.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Athena.Models;
+
+// For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Athena.Controllers
+{
+    [Route("api/todos")]
+    public class TodosController : Controller
+    {
+        public TodosController(ITodoRepository todoItems)
+        {
+            TodoItems = todoItems;
+        }
+        public ITodoRepository TodoItems { get; set; }
+
+        // GET: api/values
+        [HttpGet]
+        public IEnumerable<TodoItem> Get()
+        {
+            return TodoItems.GetAll();
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}", Name = "GetTodo")]
+        public IActionResult Get(string id)
+        {
+            var item = TodoItems.Find(id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+            return new ObjectResult(item);
+        }
+
+        // POST api/values
+        [HttpPost]
+        public IActionResult Post([FromBody]TodoItem value)
+        {
+            if (value == null)
+            {
+                return BadRequest();
+            }
+            TodoItems.Add(value);
+            return CreatedAtRoute("GetTodo", new { id = value.Key }, value);
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public IActionResult Put(string id, [FromBody]TodoItem value)
+        {
+            if (value == null || value.Key != id)
+            {
+                return BadRequest();
+            }
+
+            var todo = TodoItems.Find(id);
+            if (todo == null)
+            {
+                return NotFound();
+            }
+
+            TodoItems.Update(value);
+            return new NoContentResult();
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(string id)
+        {
+            var todo = TodoItems.Find(id);
+            if (todo == null)
+            {
+                return NotFound();
+            }
+
+            TodoItems.Remove(id);
+            return new NoContentResult();
+        }
+
+    }
+}
